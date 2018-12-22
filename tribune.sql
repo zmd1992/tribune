7a9c932ef97074f7dbb41f5c9286128c59baaf03
张梦丹 <zhangmengdan@ihappy.com>
2018-12-22 15:25:53 +0800

    pom文件修改

--- tribune.sql
+++
@@ -1,9 +0,0 @@
-create database tribune;
-use tribune;
-select * from user;
-create table my_garden(
-id int  not null auto_increment primary key comment '主键ID',
-content varchar(255) comment '内容',
-created_at datetime comment '创建时间',
-created_id datetime comment '创建人ID'
-);
